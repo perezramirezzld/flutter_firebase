c3bedd0994788d50078e982a1a3dd9ffec9d2250
Laura Perez <lpr74382@lasallebajio.edu.mx>
2023-05-15 00:09:04 -0600

    menu vacio

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 //Firebase
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_firebase/screens/general/login_screen.dart';
+import 'package:flutter_firebase/screens/menu_screen.dart';
 import 'package:flutter_firebase/screens/product/product_screen.dart';
 import 'package:flutter_firebase/screens/product/upproduct_screen.dart';
 import 'package:flutter_firebase/screens/user/user_screen.dart';
@@ -23,15 +24,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
+        '/menu': (context) => menuscreen(),
         '/updateProduct': (context) => const upproduct(),
-        '/login':(context) =>  login_Screen(),
-        '/users':(context) => user_screen(),
-        '/products':(context) =>  productscreen(),
+        '/login': (context) => login_Screen(),
+        '/users': (context) => user_screen(),
+        '/products': (context) => productscreen(),
       },
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.orange),
-      initialRoute: "/login",
+      initialRoute: "/menu",
     );
   }
 }
---
+++ lib/screens/menu_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_firebase/screens/product/product_screen.dart';
+import 'package:flutter_firebase/screens/user/user_screen.dart';
+
+class menuscreen extends StatefulWidget {
+  const menuscreen({super.key});
+
+  @override
+  State<menuscreen> createState() => _menuscreenState();
+}
+
+class _menuscreenState extends State<menuscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
